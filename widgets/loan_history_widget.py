9147662cb82203874ee549ffd7d1cd69690abcc9
Micah <micahwllmsn@gmail.com>
2025-01-05 08:56:43 -0600

    Updates formatting in loan history so it is easier to understand warnings

--- widgets/loan_history_widget.py
+++ widgets/loan_history_widget.py
@@ -1,5 +1,7 @@
 from rich.table import Table
-from textual.widgets import Static
+from textual.app import ComposeResult
+from textual.containers import Vertical
+from textual.widgets import Static, Label
 
 from models.user_data import UserData
 
@@ -10,14 +12,31 @@ class LoanHistoryWidget(Static):
         self.user_data = user_data
         super().__init__(**kwargs)
 
-    def on_mount(self) -> None:
+    def compose(self) -> ComposeResult:
+        loan_history_table = self.get_loan_history_table()
+        with Vertical():
+            yield Label(
+                'Field marked in orange are [yellow]warnings[/]. The number after the | marks the corresponding field.')
+            yield Label('')
+            yield Label(
+                '[u]Borrow Amount [1][/]: The rq borrow amount does not match the actual borrow amount. Possible difficulty in negotiating loan')
+            yield Label(
+                '[u]Borrow Date [2][/]: The post date does not match the borrowed date. Possible difficulty in negotiating loan')
+            yield Label(
+                '[u]Repay Date [3][/]: Loan was repaid after the rq repay date. Possible late payment')
+            yield Label('')
+            loan_history_container = Static()
+            loan_history_container.update(loan_history_table)
+            yield loan_history_container
+
+    def get_loan_history_table(self) -> Table:
         loan_history = self.user_data.loan_history
 
         def _highlight_inspected_user(username: str) -> str:
             return f'[bold blue]{username}[/]' if username.lower() == self.user_data.username.lower() else username
 
-        def _warn_if(value, predicate):
-            return f'[yellow]{value}[/]' if predicate() else str(value)
+        def _warn_if(value, flag: bool, match: int):
+            return f'[yellow]{value}|{match}[/]' if flag else str(value)
 
         table = Table()
         table.add_column('status')
@@ -36,25 +55,29 @@ class LoanHistoryWidget(Static):
         table.add_column('link')
 
         for row in loan_history:
+            # User did not get the loan on the same day of request
+            flag_borrow_date = row.loan_request.created_at != row.borrow_date
+            # The borrow amount change from the request
+            flag_borrow_amount = row.loan_request.borrow_amount != row.borrow_amount
+            # The actual repay date is past the proposed repay date. Repayment may have been late
+            flag_repay_date = row.loan_request.repay_date is not None and row.repaid_date is not None and row.loan_request.repay_date < row.repaid_date
+
             table.add_row(
                 '[red bold]Unpaid[/]' if row.repaid_date is None else '[green bold]Paid[/]',
                 _highlight_inspected_user(row.lender),
                 _highlight_inspected_user(row.borrower),
                 row.currency_code,
-                str(row.borrow_amount),
+                _warn_if(row.borrow_amount, flag_borrow_amount, 1),
                 str(row.repaid_amount),
-                str(row.borrow_date),
-                str(row.repaid_date),
-                _warn_if(row.loan_request.created_at,
-                         lambda: row.loan_request.created_at != row.borrow_date),
-                _warn_if(row.loan_request.borrow_amount,
-                         lambda: row.loan_request.borrow_amount != row.borrow_amount),
+                _warn_if(row.borrow_date, flag_borrow_date, 2),
+                _warn_if(row.repaid_date, flag_repay_date, 3),
+                _warn_if(row.loan_request.created_at, flag_borrow_date, 2),
+                _warn_if(row.loan_request.borrow_amount, flag_borrow_amount, 1),
                 str(row.loan_request.repay_amount),
-                _warn_if(row.loan_request.repay_date,
-                         lambda: row.loan_request.repay_date is not None and row.repaid_date is not None and row.loan_request.repay_date < row.repaid_date),
+                _warn_if(row.loan_request.repay_date, flag_repay_date, 3),
                 str(len(
                     row.loan_request.repay_installments)) if row.loan_request.repay_installments else '?',
                 f'[link={row.loan_request.permalink}]post[/]',
             )
 
-        self.update(table)
+        return table
